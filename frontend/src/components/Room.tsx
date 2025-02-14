37c0b38330377142f7e9c62137d6dd7ab241af87
manasvi001 <manasvi.d007@gmail.com>
2025-02-14 15:34:08 +0530

    Init

---
+++ frontend/src/components/Room.tsx
@@ -0,0 +1,19 @@
+/* eslint-disable @typescript-eslint/no-unused-vars */
+import { useEffect } from "react";
+import { useSearchParams } from "react-router-dom";
+
+
+export const Room = () => {
+    const [searchParams, setSearchParams] = useSearchParams();
+    const name = searchParams.get('name');
+
+    useEffect(() => {
+        // logic to init user to the room
+    }, [name]);
+
+    return (
+        <div>
+            Hi {name}
+        </div>
+    )
+}
